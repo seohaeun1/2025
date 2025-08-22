23353643aa77c89774dbeffd2b1a728682a96073
seohaeun1 <hhanana491@gmail.com>
2025-08-22 16:11:11 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ import streamlit as st
 # ------------------------
 quiz_data = [
     {"question": "다음 중 올바른 맞춤법은?", "options": ["안 되", "안돼", "않돼"], "answer": "안돼"},
-    {"question": "다음 중 올바른 맞춤법은?", "options": ["금세", "금새", "금"], "answer": "금새"},
+    {"question": "다음 중 올바른 맞춤법은?", "options": ["금세", "금새", "금쇠"], "answer": "금세"},
     {"question": "다음 중 올바른 맞춤법은?", "options": ["맞히다", "맞추다", "마추다"], "answer": "맞히다"},
     {"question": "다음 중 올바른 맞춤법은?", "options": ["왠지", "웬지", "왠진"], "answer": "왠지"},
     {"question": "다음 중 올바른 맞춤법은?", "options": ["깨끄시", "깨끗히", "깨끗이"], "answer": "깨끗이"},
     {"question": "다음 중 올바른 맞춤법은?", "options": ["설겆이", "설겆지", "설거지"], "answer": "설거지"},
     {"question": "다음 중 올바른 맞춤법은?", "options": ["어떻해", "어떡해", "어떠케"], "answer": "어떡해"},
-    {"question": "다음 중 올바른 맞춤법은?", "options": ["며칠", "몇 일", "몇"], "answer": "며칠"},
+    {"question": "다음 중 올바른 맞춤법은?", "options": ["며칠", "몇 일", "몇일"], "answer": "며칠"},
     {"question": "다음 중 올바른 맞춤법은?", "options": ["왼만하면", "왠만하면", "웬만하면"], "answer": "웬만하면"},
     {"question": "다음 중 올바른 맞춤법은?", "options": ["오랫만에", "오랜만에", "오래만에"], "answer": "오랜만에"},
     {"question": "다음 중 올바른 맞춤법은?", "options": ["잠갔다", "잠궜다", "잠깃다"], "answer": "잠갔다"},
